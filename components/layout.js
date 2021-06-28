64b150b50206db7d2a7ea91766af0aa9f5592ebe
lambertbrady <lambertbrady14@gmail.com>
2021-06-28 18:16:29 -0500

    Remove alert component

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,12 @@
-import Alert from '../components/alert'
 import Header from '../components/header'
 import Footer from '../components/footer'
 import Meta from '../components/meta'
 
-export default function Layout({ preview, children }) {
+export default function Layout({ children }) {
   return (
     <>
       <Meta />
       <div className="min-h-screen">
-        <Alert preview={preview} />
         <Header />
         <main>{children}</main>
         <Footer />
